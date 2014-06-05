a2127a8687caccfa5d4425f3dc55df63b7ef16d6
THEVT <rflynn32@gmail.com>
2014-06-04 18:09:40 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitHubWork::Application.config.secret_key_base = 'ff71bc0ab613165dc2913c620401345ed3b0da84847046cfda0662e38abe6841905ce8e7448c972aa2bd470429ec5beb1e48eba8cb568d1545af72630c56c2dc'
